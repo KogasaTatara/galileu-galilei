0e074913a381dee5beef4d04c6797f1cd5656014
KogasaTatara <tanuke.p2@gmail.com>
2025-09-15 11:51:55 -0300

    coocooo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { firebaseConfig } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -7,10 +8,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { intializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment.prod';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => intializeApp(environment.firebaseConfig)),
+    provideFirestore (() => getFirestore())
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,15 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkzoqKqZbdB89sbn3BL3T4z4xTjcjWQCk",
+  authDomain: "galileu-galilei.firebaseapp.com",
+  projectId: "galileu-galilei",
+  storageBucket: "galileu-galilei.firebasestorage.app",
+  messagingSenderId: "781382671840",
+  appId: "1:781382671840:web:64c7cc9e708e363f5d5f85",
+  measurementId: "G-95N070Q76J"
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
