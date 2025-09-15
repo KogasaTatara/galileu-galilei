b324049aadbba62608f18b447fa092ab6dc03abf
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-15 11:20:14 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Galileu Galilei',
+  webDir: 'www'
+};
+
+export default config;
