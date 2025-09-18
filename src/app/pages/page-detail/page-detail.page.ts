1f0c12397473b0c448589a863e4a137eb9c76924
KogasaTatara <tanuke.p2@mail.com>
2025-09-18 11:32:47 -0300

    mó trampo

---
+++ src/app/pages/page-detail/page-detail.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-detail',
+  templateUrl: './page-detail.page.html',
+  styleUrls: ['./page-detail.page.scss'],
+})
+export class PageDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
