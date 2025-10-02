2dee28978de0c44c7a969bba50cc121c5cce22d9
KogasaTatara <tanuke.p2@gmail.com>
2025-10-01 21:43:14 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ac1-Parte1-Tri3',
+  webDir: 'www'
+};
+
+export default config;
