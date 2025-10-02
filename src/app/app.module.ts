b3d5e0601b6d46187e93a1bbfb67386b8ee3a18c
KogasaTatara <tanuke.p2@gmail.com>
2025-10-01 23:47:31 -0300

    engraçado né? como as coisas funcionam...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { firebaseConfig } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -7,10 +8,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore (() => getFirestore())
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { DataService, Item } from '../services/data';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,6 +11,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items: Item[] = [];
+  constructor(
+    private dataService: DataService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
+  ngOnInit(){
+    this.dataService.getItems ().subscribe(res => {this.items = res;});
+  }
+  addItem(){
+    this.router.navigateByUrl('/page-detail');
+  }
+  editItem(item: Item){
+    this.router.navigateByUrl(`/page-detail/${item.id}`);
+  }
+  async deleteItem(id: string) {
+    const alert = await this.alertController.create({
+      header: 'Confirmar exclusão',
+      message: 'Tem certesa que deseja excluir esse item?',
+      buttons:[
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Excluir',
+          handler: () => {
+            this.dataService.deleteItem(id);
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
