1410a42d94baf8ea2d8e6c67da5a628ffcf2639d
KogasaTatara <tanuke.p2@gmail.com>
2025-10-01 23:49:34 -0300

    Apertei muito o Tab para isso

--- src/app/pages/page-detail/page-detail.page.ts
+++ src/app/pages/page-detail/page-detail.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute, Router } from '@angular/router';
-import { DataService, Item } from '../../services/data.service';
+import { DataService, Item } from '../../services/data';
 import { LoadingController, ToastController } from '@ionic/angular';
 
 
@@ -33,6 +33,65 @@ export class PageDetailPage implements OnInit {
       this.isNewItem = false;
 
       this.loadItem();
+
+    }
+  }
+  async loadItem() {
+    const loading = await this.LoadingController.create({
+      message: 'Carregando item...'
+    });
+    await loading.present();
+    this.dataService.getItem(this.itemId!).subscribe(res => {
+      loading.dismiss();
+     if (res) {
+      this.item = res;
+     } else {
+      this.presentToast('Item não encontrado', 'danger');
+      this.router.navigateByUrl('/home');
+     }
+    }, err => {
+      loading.dismiss();
+      this.presentToast('Erro ao carregar item', 'danger');
+      this.router.navigateByUrl('/home');
+    });
+  }
+
+  async saveItem() {
+    const loading = await this.LoadingController.create({
+      message: 'Salvando item...'
+    });
+    await loading.present();
+
+    if (this.isNewItem) {
+      this.dataService.addItem(this.item).then(() => {
+        loading.dismiss();
+        this.presentToast('Item adicionado com sucesso', 'success');
+        this.router.navigateByUrl('/home');
+      }, err => {
+        loading.dismiss();
+        this.presentToast('Erro ao adicionar item', 'danger');
+      });
+}   else {
+      this.dataService.updateItem(this.item).then(() => {
+        loading.dismiss();
+        this.presentToast('Item atualizado com sucesso', 'success');
+        this.router.navigateByUrl('/home');
+      }, err => {
+        loading.dismiss();
+        this.presentToast('Erro ao atualizar item', 'danger');
+      });
+}
+}
+  async presentToast(message: string, color: string = 'primary') {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: color
+    });
+    toast.present();
+  }
+}
+
 
     }
   }
